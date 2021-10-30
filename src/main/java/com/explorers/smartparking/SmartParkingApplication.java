dc644a6825a39e3d268f31cecd12e7a5cd0cf276
DaNaRim <rangar.danarim@gmail.com>
2021-10-30 12:47:23 +0300

    init commit

---
+++ src/main/java/com/explorers/smartparking/SmartParkingApplication.java
@@ -0,0 +1,13 @@
+package com.explorers.smartparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartParkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartParkingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/explorers/smartparking/SmartParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.explorers.smartparking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartParkingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
