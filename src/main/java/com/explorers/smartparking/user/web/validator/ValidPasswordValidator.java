c20bef5bb05b8305bef938aa57134fd8975d6060
DaNaRim <rangar.danarim@gmail.com>
2021-12-20 12:13:06 +0200

    simplify password validation

--- src/main/java/com/explorers/smartparking/user/web/validator/ValidPasswordValidator.java
+++ src/main/java/com/explorers/smartparking/user/web/validator/ValidPasswordValidator.java
@@ -18,9 +18,9 @@ public class ValidPasswordValidator implements ConstraintValidator<ValidPassword
     public boolean isValid(String password, ConstraintValidatorContext context) {
         PasswordValidator validator = new PasswordValidator(Arrays.asList(
                 new LengthRule(8, 30),
-                new UppercaseCharacterRule(1),
-                new DigitCharacterRule(1),
-                new SpecialCharacterRule(1),
+//                new UppercaseCharacterRule(1),
+//                new DigitCharacterRule(1),
+//                new SpecialCharacterRule(1),
                 new WhitespaceRule()));
         RuleResult result = validator.validate(new PasswordData(password));
         if (result.isValid()) return true;
