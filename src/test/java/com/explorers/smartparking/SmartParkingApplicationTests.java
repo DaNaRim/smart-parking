df86d43cfd6b122f6cf4c936ec00940ca0005760
DaNaRim <rangar.danarim@gmail.com>
2022-09-30 02:02:28 +0300

    UPDATE improve main Application Test

--- src/test/java/com/explorers/smartparking/SmartParkingApplicationTests.java
+++ src/test/java/com/explorers/smartparking/SmartParkingApplicationTests.java
@@ -2,12 +2,16 @@ package com.explorers.smartparking;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class SmartParkingApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads(ApplicationContext context) {
+        assertThat(context).isNotNull();
     }
 
 }
