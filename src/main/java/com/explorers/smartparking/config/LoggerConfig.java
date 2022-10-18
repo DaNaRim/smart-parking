52b80d1fe95077e29eb2a4791847af32d3cced11
DaNaRim <rangar.danarim@gmail.com>
2022-10-18 18:55:04 +0300

    UPDATE add smtp logger and logging improvements

--- src/main/java/com/explorers/smartparking/config/LoggerConfig.java
+++ src/main/java/com/explorers/smartparking/config/LoggerConfig.java
@@ -1,48 +1,60 @@
 package com.explorers.smartparking.config;
 
+import com.google.common.collect.Maps;
 import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.core.Filter;
 import org.apache.logging.log4j.core.LoggerContext;
-import org.apache.logging.log4j.core.appender.ConsoleAppender;
 import org.apache.logging.log4j.core.config.Configuration;
 import org.apache.logging.log4j.core.config.ConfigurationFactory;
 import org.apache.logging.log4j.core.config.ConfigurationSource;
 import org.apache.logging.log4j.core.config.Order;
 import org.apache.logging.log4j.core.config.builder.api.AppenderComponentBuilder;
 import org.apache.logging.log4j.core.config.builder.api.ConfigurationBuilder;
+import org.apache.logging.log4j.core.config.builder.api.LoggerComponentBuilder;
 import org.apache.logging.log4j.core.config.builder.api.RootLoggerComponentBuilder;
 import org.apache.logging.log4j.core.config.builder.impl.BuiltConfiguration;
 import org.apache.logging.log4j.core.config.plugins.Plugin;
 
+import java.io.FileInputStream;
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Properties;
+
+import static org.apache.logging.log4j.core.appender.ConsoleAppender.Target.SYSTEM_OUT;
 
 @Plugin(name = "CustomConfigurationFactory", category = ConfigurationFactory.CATEGORY)
 @Order(50)
 public class LoggerConfig extends ConfigurationFactory {
 
     private static final String LOGS_FOLDER = "./logs/";
-    private static final Level ROOT_LEVEL = Level.ALL;
 
     private static final String CONSOLE_APPENDER = "CONSOLE";
     private static final String CONSOLE_PATTERN
             = "%d %highlight{%-5p} [%15.15thread] %style{%40.40logger{1}}{blue} : %m %throwable{short}%n";
-    private static final ConsoleAppender.Target CONSOLE_TARGET = ConsoleAppender.Target.SYSTEM_OUT;
     private static final Level CONSOLE_LEVEL = Level.INFO;
 
     private static final String ROLLING_FILE_APPENDER = "ROLLING_FILE";
     private static final String ROLLING_FILE_NAME = "rolling.log";
+    private static final String ROLLING_FILE_FOLDER = LOGS_FOLDER + "rolling-%d{yyyy-MM-dd}/";
     private static final String ROLLING_FILE_NAME_PATTERN = "rolling-%d{yyyy-MM-dd}-%i.log.gz";
     private static final String ROLLING_FILE_INTERVAL = "1";
     private static final String ROLLING_FILE_PATTERN
             = "%d %highlight{%-5p} [%20.20thread] %style{%40.40logger{3.}}{blue} : %m %n%throwable";
     private static final String ROLLING_FILE_MAX_SIZE = "100MB";
-    private static final String ROLLING_FILE_MAX_COUNT = "5";
+    private static final String ROLLING_FILE_MAX_COUNT = "10";
     private static final Level ROLLING_FILE_LEVEL = Level.DEBUG;
 
     private static final String FILE_APPENDER = "FILE";
-    private static final String FILE_NAME = "log.log";
+    private static final String FILE_NAME = "smart-parking.log";
     private static final String FILE_PATTERN
             = "%d %highlight{%-5p} [%15.15thread] %style{%40.40logger{1}}{blue} : %m %n%throwable{5}";
     private static final Level FILE_LEVEL = Level.INFO;
+
+    private static final String SMTP_APPENDER = "SMTP";
+    private static final String SMTP_SUBJECT = "Smart Parking Error";
+    private static final String SMTP_TO = "rangar.danarim@gmail.com";
+    private static final String SMTP_PATTERN = "%d%n %m%n %throwable";
+    private static final Level SMTP_LEVEL = Level.ERROR;
 
     private static Configuration createConfiguration(final String name,
                                                      final ConfigurationBuilder<BuiltConfiguration> builder) {
@@ -53,20 +65,33 @@ public class LoggerConfig extends ConfigurationFactory {
         builder.add(getConsoleAppender(builder));
         builder.add(getRollingFileAppender(builder));
         builder.add(getFileAppender(builder));
+        builder.add(getSmtpAppender(builder));
 
-        RootLoggerComponentBuilder rootLogger = builder.newRootLogger(ROOT_LEVEL)
+        System.setProperty("mail.smtp.starttls.enable", "true");
+
+        RootLoggerComponentBuilder rootLogger = builder.newAsyncRootLogger(Level.ALL)
+                .add(builder.newFilter("ThresholdFilter", Filter.Result.ACCEPT, Filter.Result.NEUTRAL)
+                        .addAttribute("level", Level.ALL)
+                )
                 .add(builder.newAppenderRef(CONSOLE_APPENDER).addAttribute("level", CONSOLE_LEVEL))
                 .add(builder.newAppenderRef(FILE_APPENDER).addAttribute("level", FILE_LEVEL))
                 .add(builder.newAppenderRef(ROLLING_FILE_APPENDER).addAttribute("level", ROLLING_FILE_LEVEL));
 
+        LoggerComponentBuilder asyncLogger = builder.newAsyncLogger("com", Level.ALL)
+                .add(builder.newFilter("ThresholdFilter", Filter.Result.ACCEPT, Filter.Result.NEUTRAL)
+                        .addAttribute("level", Level.ALL)
+                )
+                .add(builder.newAppenderRef(SMTP_APPENDER).addAttribute("level", SMTP_LEVEL));
+
         builder.add(rootLogger);
+        builder.add(asyncLogger);
 
         return builder.build();
     }
 
     private static AppenderComponentBuilder getConsoleAppender(ConfigurationBuilder<BuiltConfiguration> builder) {
         return builder.newAppender(CONSOLE_APPENDER, "Console")
-                .addAttribute("target", CONSOLE_TARGET)
+                .addAttribute("target", SYSTEM_OUT)
                 .add(builder.newLayout("PatternLayout")
                         .addAttribute("pattern", CONSOLE_PATTERN)
                         .addAttribute("disableAnsi", "false")
@@ -76,7 +101,7 @@ public class LoggerConfig extends ConfigurationFactory {
     private static AppenderComponentBuilder getRollingFileAppender(ConfigurationBuilder<BuiltConfiguration> builder) {
         return builder.newAppender(ROLLING_FILE_APPENDER, "RollingFile")
                 .addAttribute("fileName", LOGS_FOLDER + ROLLING_FILE_NAME)
-                .addAttribute("filePattern", LOGS_FOLDER + ROLLING_FILE_NAME_PATTERN)
+                .addAttribute("filePattern", ROLLING_FILE_FOLDER + ROLLING_FILE_NAME_PATTERN)
                 .add(builder.newLayout("PatternLayout")
                         .addAttribute("pattern", ROLLING_FILE_PATTERN)
                         .addAttribute("disableAnsi", "false")
@@ -101,6 +126,37 @@ public class LoggerConfig extends ConfigurationFactory {
                         .addAttribute("pattern", FILE_PATTERN)
                         .addAttribute("disableAnsi", "false")
                 );
+    }
+
+    private static AppenderComponentBuilder getSmtpAppender(ConfigurationBuilder<BuiltConfiguration> builder) {
+        HashMap<String, String> properties = getSecurityProperties();
+        String username = properties.get("secrets.mail-username");
+        String password = properties.get("secrets.mail-password");
+
+        return builder.newAppender(SMTP_APPENDER, "SMTP")
+                .addAttribute("subject", SMTP_SUBJECT)
+                .addAttribute("to", SMTP_TO)
+                .addAttribute("from", username)
+                .addAttribute("smtpHost", "smtp.gmail.com")
+                .addAttribute("smtpPort", "587")
+                .addAttribute("smtpUsername", username)
+                .addAttribute("smtpPassword", password)
+                .addAttribute("smtpProtocol", "smtp")
+                .add(builder.newLayout("PatternLayout")
+                        .addAttribute("pattern", SMTP_PATTERN)
+                );
+    }
+
+    private static HashMap<String, String> getSecurityProperties() {
+        try {
+            Properties properties = new Properties();
+
+            properties.load(new FileInputStream("src/main/resources/secrets.properties"));
+
+            return Maps.newHashMap(Maps.fromProperties(properties));
+        } catch (Exception e) {
+            throw new RuntimeException("Error while reading security.properties", e);
+        }
     }
 
     @Override
